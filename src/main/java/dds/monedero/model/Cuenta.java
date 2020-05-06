fc67bde3943a1b03788d028c3e272d75ea748e9e
Damuchi99 <damuchi99@gmail.com>
2020-05-06 19:29:41 -0300

    Areglo el metodo poner

--- src/main/java/dds/monedero/model/Cuenta.java
+++ src/main/java/dds/monedero/model/Cuenta.java
@@ -28,19 +28,18 @@ public class Cuenta {
   }
 
   public void poner(double cuanto) {
-    if (cuanto <= 0) {
-      throw new MontoNegativoException(cuanto + ": el monto a ingresar debe ser un valor positivo");
-    }
+	validarMontoNegativo(cuanto);
     
-    //si se hace esta comparacion en el filter, entonces el metodo fueDepositado(fecha) estaria al pedo
-  	//¿por qué no usar dicho metodo?
-    //Aparte que en este if no se pregunta si el depósito fue de la fecha de hoy
-    //Toda esta comparacion del if la podriamos meter en un metodo
-    if (getMovimientos().stream().filter(movimiento -> movimiento.isDeposito()).count() >= 3) {
+    if (getDepositosDeFecha(LocalDate.now()).count() >= 3) {
       throw new MaximaCantidadDepositosException("Ya excedio los " + 3 + " depositos diarios");
     }
 
     new Deposito(LocalDate.now(), cuanto).agregateA(this);
+  }
+  
+  public Stream<Movimiento> getDepositosDeFecha(LocalDate fecha){
+	  return getMovimientos().stream()
+		.filter(movimiento -> movimiento.isDeposito() && movimiento.getFecha().equals(fecha));
   }
   
   public void sacar(double cuanto) {
